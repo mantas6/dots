7a12780fe15c922817c9b9dc082b2488b8eac271
Mantas <6524518+mantas6@users.noreply.github.com>
2024-04-02 11:11:49 +0300

    Tinkering

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,3 +10,5 @@ vim.opt.number = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
+
+vim.g.mapleader = " "
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -5,6 +5,19 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use {
       'VonHeikemen/lsp-zero.nvim',
       branch = 'v3.x',
       requires = {
