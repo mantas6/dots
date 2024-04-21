a03445b74239a003411fc190ec4c7055f05a94f0
Mantas <6524518+mantas6@users.noreply.github.com>
2024-04-21 08:57:10 +0300

    Current tags

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -199,7 +199,7 @@ awful.screen.connect_for_each_screen(function(s)
     -- Create a tasklist widget
     s.mytasklist = awful.widget.tasklist {
         screen  = s,
-        filter  = awful.widget.tasklist.filter.alltags,
+        filter  = awful.widget.tasklist.filter.currenttags,
         buttons = tasklist_buttons,
         widget_template = {
             {
