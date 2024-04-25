2443138373c62206c758119f2e1daa8cf6491b4c
Mantas <6524518+mantas6@users.noreply.github.com>
2024-04-25 14:46:24 +0300

    Update

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -68,15 +68,15 @@ modkey = "Mod4"
 awful.layout.layouts = {
     awful.layout.suit.tile,
     -- awful.layout.suit.tile.left,
-    -- awful.layout.suit.tile.bottom,
-    -- awful.layout.suit.tile.top,
+    awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.top,
     -- awful.layout.suit.fair,
     -- awful.layout.suit.fair.horizontal,
     -- awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,
     -- awful.layout.suit.max,
-    -- awful.layout.suit.max.fullscreen,
-    -- awful.layout.suit.magnifier,
+    awful.layout.suit.max.fullscreen,
+    awful.layout.suit.magnifier,
     -- awful.layout.suit.corner.nw,
     -- awful.layout.suit.floating,
     -- awful.layout.suit.corner.ne,
@@ -231,7 +231,7 @@ awful.screen.connect_for_each_screen(function(s)
     }
 
     -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", height = 22, screen = s })
+    s.mywibox = awful.wibar({ position = "top", height = 24, screen = s })
 
     -- Add widgets to the wibox
     s.mywibox:setup {
