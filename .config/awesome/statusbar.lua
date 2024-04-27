946212c3e4ef1b98500b63f8fb0be39913c4ee38
Mantas <6524518+mantas6@users.noreply.github.com>
2024-04-27 12:58:24 +0300

    Bar

--- .config/awesome/statusbar.lua
+++ .config/awesome/statusbar.lua
@@ -11,7 +11,24 @@ module.clock = wibox.widget.textclock("%a %d %H:%M:%S", 1)
 module.ping = awful.widget.watch("bash -c \"echo -n '    '&& ping -c 1 google.com | grep 'time=' | awk -F 'time=' '{print $2}'\"", 5);
 
 module.ping = createWidget("ping -c 1 google.com | grep 'time=' | awk -F 'time=' '{print $2}'", '')
-module.battery = createWidget("echo $(</sys/class/power_supply/BAT0/capacity)%", '󱐋')
+
+-- Battery
+
+local batteryPath = '/sys/class/power_supply/BAT0'
+module.battery = createWidget("echo $(<"..batteryPath.."/capacity):$(<"..batteryPath.."/status)", function(params)
+    local percentage = string.match(params.text, "([^:]+)")
+    local status = string.match(params.text, ":(.+)"):match("^%s*(.-)%s*$")
+
+    if status == 'Not charging' then
+        params.icon = ''
+    else
+        params.icon = '󱐋'
+    end
+
+    params.text = percentage..'%'
+end)
+
+
 module.memory = createWidget("free -h | awk '/Mem:/ {print $3}'", '󰘚')
 module.load = createWidget("cut -d ' ' -f1 < /proc/loadavg", '')
 
