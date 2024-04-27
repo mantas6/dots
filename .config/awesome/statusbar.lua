60dd0104d8e499818c87ffec03460c04b3cda672
Mantas <6524518+mantas6@users.noreply.github.com>
2024-04-27 12:34:45 +0300

    Update

--- .config/awesome/statusbar.lua
+++ .config/awesome/statusbar.lua
@@ -1,5 +1,6 @@
 local awful = require("awful")
 local wibox = require("wibox")
+local createWidget = require("statusbar-widget")
 
 local module = {};
 
@@ -7,13 +8,12 @@ module.keyboard = awful.widget.keyboardlayout()
 
 module.clock = wibox.widget.textclock("%a %d %H:%M:%S", 1)
 
-module.ping = awful.widget.watch("bash -c \"echo -n '    '&& ping -c 1 google.com | grep 'time=' | awk -F 'time=' '{print $2}'\"", 5);
+module.ping = awful.widget.watch("bash -c \"echo -n '    '&& ping -c 1 google.com | grep 'time=' | awk -F 'time=' '{print $2}'\"", 5);
 
-module.battery = awful.widget.watch('bash -c \'echo " 󱐋 " $(</sys/class/power_supply/BAT0/capacity)%\' ', 15);
-
-module.load = awful.widget.watch("bash -c \"echo -n '    ' && cut -d ' ' -f1 < /proc/loadavg\"", 5);
-
-module.memory = awful.widget.watch("bash -c \"echo -n ' 󰘚  ' && free -h | awk '/Mem:/ {print $3}'\"", 5);
+module.ping = createWidget("ping -c 1 google.com | grep 'time=' | awk -F 'time=' '{print $2}'", '')
+module.battery = createWidget("echo $(</sys/class/power_supply/BAT0/capacity)%", '󱐋')
+module.memory = createWidget("free -h | awk '/Mem:/ {print $3}'", '󰘚')
+module.load = createWidget("cut -d ' ' -f1 < /proc/loadavg", '')
 
 -- cat /proc/loadavg | cut -d ' ' -f1
 -- free -h | awk '/Mem:/ {print $3}'
