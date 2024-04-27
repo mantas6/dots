cabb5cf561e2830993cac33b006aaf6542825b68
Mantas <6524518+mantas6@users.noreply.github.com>
2024-04-27 14:01:29 +0300

    Status

--- .config/awesome/statusbar.lua
+++ .config/awesome/statusbar.lua
@@ -8,21 +8,33 @@ module.keyboard = awful.widget.keyboardlayout()
 
 module.clock = wibox.widget.textclock("%a %d %H:%M:%S", 1)
 
-module.ping = awful.widget.watch("bash -c \"echo -n '    '&& ping -c 1 google.com | grep 'time=' | awk -F 'time=' '{print $2}'\"", 5);
-
 module.ping = createWidget("ping -c 1 google.com | grep 'time=' | awk -F 'time=' '{print $2}'", '')
 
 -- Battery
 
 local batteryPath = '/sys/class/power_supply/BAT0'
-module.battery = createWidget("echo $(<"..batteryPath.."/capacity):$(<"..batteryPath.."/status)", function(params)
+module.battery = createWidget("echo -n $(<"..batteryPath.."/capacity):$(<"..batteryPath.."/status)", function(params)
     local percentage = string.match(params.text, "([^:]+)")
     local status = string.match(params.text, ":(.+)"):match("^%s*(.-)%s*$")
 
     if status == 'Not charging' then
         params.icon = ''
-    else
+    elseif status == 'Charging' then
         params.icon = '󱐋'
+    else
+        local percentNum = tonumber(percentage)
+
+        if percentNum > 80 then
+            params.icon = '' -- full
+        elseif percentNum > 60 then
+            params.icon = '' -- 2/3
+        elseif percentNum > 40 then
+            params.icon = '' -- 1/2
+        elseif percentNum > 20 then
+            params.icon = '' -- 1/3
+        else
+            params.icon = '' -- 0
+        end
     end
 
     params.text = percentage..'%'
