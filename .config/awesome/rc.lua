fd86a79364a46caa605cc60602bef75e0b9921bc
Mantas <6524518+mantas6@users.noreply.github.com>
2024-04-27 09:51:09 +0300

    Fix

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -248,6 +248,7 @@ awful.screen.connect_for_each_screen(function(s)
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
             statusbar.load,
+            statusbar.memory,
             statusbar.ping,
             statusbar.battery,
             statusbar.keyboard,
