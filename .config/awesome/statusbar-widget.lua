434d90fff71bd532a666d3b0d5924f9375420635
Mantas <6524518+mantas6@users.noreply.github.com>
2024-04-28 08:11:16 +0300

    Updates applet

--- .config/awesome/statusbar-widget.lua
+++ .config/awesome/statusbar-widget.lua
@@ -2,21 +2,22 @@ local awful = require("awful")
 local wibox = require("wibox")
 local gears = require("gears")
 
-local function createWidget(command, formatter)
+local function createWidget(command, formatter, timeout)
     local widget = wibox.widget.textbox();
+    timeout = timeout or 5
 
     gears.timer {
-        timeout   = 5,
+        timeout   = timeout,
         call_now  = true,
         autostart = true,
         callback  = function()
             awful.spawn.easy_async(
             {"sh", "-c", command},
-                function(out)
+                function(out, _err, _reason, exitCode)
                     local params = {
                         color = '#bfbfbf',
                         icon = '󰕟',
-                        text = out,
+                        text = exitCode == 0 and out or '-',
                     }
 
                     if type(formatter) == 'function' then
