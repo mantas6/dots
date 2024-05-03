6fa464c5bb04a505aaaaa4496fa27a49f3e461b9
Mantas <6524518+mantas6@users.noreply.github.com>
2024-05-03 12:17:24 +0300

    Add harpoon

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -33,3 +33,7 @@ vim.keymap.set('n', '<leader>p', builtin.find_files, {})
 vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
 
 vim.keymap.set('n', '<leader>s', ':NvimTreeToggle<CR>', {})
+
+
+-- if place up to, hotkey doesnt work
+require("harpoon_config")
