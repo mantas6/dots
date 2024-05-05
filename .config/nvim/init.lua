de1d0539a1f97a276785ef3988f5631d288bf8d0
Mantas <6524518+mantas6@users.noreply.github.com>
2024-05-05 13:34:18 +0300

    Add buffers view

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -31,6 +31,7 @@ vim.g.mapleader = " "
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>p', builtin.find_files, {})
 vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 
 vim.keymap.set('n', '<leader>s', ':NvimTreeToggle<CR>', {})
 
