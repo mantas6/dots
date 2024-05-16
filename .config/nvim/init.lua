2ac9bd2de9f0be3f98e36c5c50f8918fb840d018
Mantas <6524518+mantas6@users.noreply.github.com>
2024-05-16 08:36:21 +0300

    Attempt to improve readability

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,7 +20,10 @@ vim.opt.hlsearch = false
 
 vim.api.nvim_set_option("clipboard", "unnamed")
 
+vim.o.termguicolors = true
 vim.g.neon_style = "default"
+vim.g.neon_italic_keyword = true
+vim.g.neon_italic_function = true
 vim.g.neon_transparent = true
 vim.cmd[[colorscheme neon]]
 
