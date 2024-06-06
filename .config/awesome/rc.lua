49e82ee1068968c0e7928d7864cbf1d643798c00
Mantas <6524518+mantas6@users.noreply.github.com>
2024-06-06 14:34:59 +0300

    Statusbar

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -253,9 +253,10 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            statusbar.track,
+            -- statusbar.track,
             statusbar.journal,
             statusbar.updates,
+            statusbar.cpuf,
             statusbar.load,
             statusbar.memory,
             statusbar.ping,
