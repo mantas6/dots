c4e2c7cc816ebafd173d0ba7c78859d192b21b73
mantas6 <6524518+mantas6@users.noreply.github.com>
2024-06-25 12:56:33 +0300

    Macos env, docker aliases, migrate nvim to lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,17 @@
-require("plugins")
-require("tree")
-require("lsp")
-require("treesitter")
-require("command")
+require("config.lazy")
 
--- mv to dedicated file later
-require('colorizer').setup()
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local group = augroup('MyAutoCommands', { clear = true })
+
+autocmd('BufEnter', {
+    group = group,
+    callback = function()
+        vim.opt.autoindent = true
+        vim.opt.smartindent = true
+    end
+})
 
 vim.opt.scrolloff = 8
 
@@ -23,12 +29,6 @@ vim.opt.hlsearch = false
 
 vim.api.nvim_set_option("clipboard", "unnamedplus")
 
-vim.o.termguicolors = true
-vim.g.neon_style = "default"
-vim.g.neon_italic_keyword = true
-vim.g.neon_italic_function = true
-vim.g.neon_transparent = true
-vim.cmd[[colorscheme neon]]
 
 vim.opt.spell = true
 vim.opt.spelllang = { 'en_us' }
@@ -37,16 +37,7 @@ vim.opt.spelllang = { 'en_us' }
 -- TODO: move to separate file
 vim.g.mapleader = " "
 
--- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pp', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pf', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 
 -- NvimTree
 vim.keymap.set('n', '<leader>ss', ':NvimTreeToggle<CR>', {})
 vim.keymap.set('n', '<leader>sr', ':NvimTreeFindFile<CR>', {})
-
-
--- if place up to, hotkey doesnt work
-require("bookmarks")
