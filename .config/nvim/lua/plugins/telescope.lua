93a4ce86a2e1e4ac3429e46eaadbf6a183e4afbc
mantas6 <6524518+mantas6@users.noreply.github.com>
2024-07-18 13:32:20 +0300

    Telescope keybind

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -15,6 +15,7 @@ return {
         vim.keymap.set('n', '<leader>pf', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>pw', builtin.grep_string, {})
         vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>ph', builtin.oldfiles, {})
         vim.keymap.set('n', '<leader>ps', builtin.git_status, {})
         vim.keymap.set('n', '<leader>pr', builtin.resume, {})
 
