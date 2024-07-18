e54519160071b20e19b2e020b5ce05cf455cd0ba
mantas6 <6524518+mantas6@users.noreply.github.com>
2024-07-18 08:22:43 +0300

    Add more telescope builtins

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -13,8 +13,12 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pp', builtin.find_files, {})
         vim.keymap.set('n', '<leader>pf', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>pw', builtin.grep_string, {})
         vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>ps', builtin.git_status, {})
         vim.keymap.set('n', '<leader>pr', builtin.resume, {})
+
+        vim.keymap.set('n', '<leader>p"', builtin.registers, {})
+        vim.keymap.set('n', '<leader>pq', builtin.command_history, {})
     end
 }
