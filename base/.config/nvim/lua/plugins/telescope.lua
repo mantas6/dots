57da906969c64948d38eed17a8217d5c66630cfd
mantas6 <6524518+mantas6@users.noreply.github.com>
2024-08-08 09:29:09 +0300

    Add file dir telescope shortcut, switch indentation to 2

--- base/.config/nvim/lua/plugins/telescope.lua
+++ base/.config/nvim/lua/plugins/telescope.lua
@@ -1,25 +1,32 @@
 return {
-    "nvim-telescope/telescope.nvim",
+  "nvim-telescope/telescope.nvim",
 
-    tag = '0.1.6',
+  tag = '0.1.6',
 
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
 
-    config = function()
-        require('telescope').setup({})
+  config = function()
+    require('telescope').setup({})
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pp', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>pf', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>pw', builtin.grep_string, {})
-        vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>ph', builtin.oldfiles, {})
-        vim.keymap.set('n', '<leader>ps', builtin.git_status, {})
-        vim.keymap.set('n', '<leader>pr', builtin.resume, {})
+    local builtin = require('telescope.builtin')
+    local utils = require("telescope.utils")
 
-        vim.keymap.set('n', '<leader>p"', builtin.registers, {})
-        vim.keymap.set('n', '<leader>pq', builtin.command_history, {})
-    end
+    vim.keymap.set('n', '<leader>pp', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>pf', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>pd', function ()
+      builtin.find_files({ cwd = utils.buffer_dir() })
+    end)
+
+    vim.keymap.set('n', '<leader>pw', builtin.grep_string, {})
+
+    vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>ph', builtin.oldfiles, {})
+    vim.keymap.set('n', '<leader>ps', builtin.git_status, {})
+    vim.keymap.set('n', '<leader>pr', builtin.resume, {})
+
+    vim.keymap.set('n', '<leader>p"', builtin.registers, {})
+    vim.keymap.set('n', '<leader>pq', builtin.command_history, {})
+  end
 }
