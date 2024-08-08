98c8ea1ff3116d53c08a326d2f4f179dbc7d2d50
mantas6 <6524518+mantas6@users.noreply.github.com>
2024-08-08 11:45:44 +0300

    Improve gitsigns config

--- base/.config/nvim/lua/plugins/git.lua
+++ base/.config/nvim/lua/plugins/git.lua
@@ -1,6 +1,47 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function ()
-        require('gitsigns').setup()
-    end
+  "lewis6991/gitsigns.nvim",
+
+  config = function ()
+    require('gitsigns').setup({
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation through hunks
+        map('n', ']c', function()
+          if vim.wo.diff then return ']c' end
+          vim.schedule(function() gs.next_hunk() end)
+          return '<Ignore>'
+        end, {expr=true})
+
+        -- Navigation through hunks
+        map('n', '[c', function()
+          if vim.wo.diff then return '[c' end
+          vim.schedule(function() gs.prev_hunk() end)
+          return '<Ignore>'
+        end, {expr=true})
+
+        -- Actions
+        map('n', '<leader>gS', gs.stage_buffer)
+        map('n', '<leader>ga', gs.stage_hunk)
+        map('n', '<leader>gu', gs.undo_stage_hunk)
+        map('n', '<leader>gR', gs.reset_buffer)
+        map('n', '<leader>gp', gs.preview_hunk)
+        map('n', '<leader>gv', ':<C-U>Gitsigns select_hunk<CR>')
+      end
+    })
+  end
 }
