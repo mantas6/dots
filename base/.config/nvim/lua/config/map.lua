8ed9c230e0602eb5f4bf66aa09f94ea611be80f2
mantas6 <6524518+mantas6@users.noreply.github.com>
2024-09-16 15:03:59 +0300

    Toggl, :W map

--- base/.config/nvim/lua/config/map.lua
+++ base/.config/nvim/lua/config/map.lua
@@ -10,3 +10,6 @@ end
 
 -- Map <leader>h to toggle search highlighting
 vim.api.nvim_set_keymap('n', '<leader>h', ':lua ToggleHighlightSearch()<CR>', { noremap = true, silent = true })
+
+-- alias :W to :w
+vim.api.nvim_command('command W write')
