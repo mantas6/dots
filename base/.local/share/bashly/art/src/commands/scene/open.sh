4d5a0d26e1054f12ed7a9ad03090a54bd928656f
Mantas <6524518+mantas6@users.noreply.github.com>
2024-09-26 11:03:46 +0300

    Update

---
+++ base/.local/share/bashly/art/src/commands/scene/open.sh
@@ -0,0 +1,10 @@
+scene=${args[scene]}
+
+path="$ART_CURRENT_PATH/Scenes/$scene"
+
+cd "$path" || exit 1
+
+latest=$(find . -maxdepth 1 -name "$scene*.ntp" | sort -V | tail -n 1)
+
+wmspawn "Natron $path/$latest"
+
