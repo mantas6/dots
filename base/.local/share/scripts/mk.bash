5b1efb4a2c30e273569e7568d005db3c7ec81f28
Mantas <6524518+mantas6@users.noreply.github.com>
2024-09-28 04:31:33 +0300

    Scripts

--- base/.local/share/scripts/mk.bash
+++ base/.local/share/scripts/mk.bash
@@ -4,4 +4,7 @@
 
 echo '#!/bin/bash' >> "$1"
 chmod +x "$1"
+
+[ "$2" = '-e' ] && nvim "$1"
+
 exit 0
--- base/.local/share/scripts/mk.sh
+++ base/.local/share/scripts/mk.sh
@@ -4,4 +4,7 @@
 
 echo '#!/bin/sh' >> "$1"
 chmod +x "$1"
+
+[ "$2" = '-e' ] && nvim "$1"
+
 exit 0
