5a5040b1f225514bbb3b6be3565e3c00e4d257b4
Mantas <6524518+mantas6@users.noreply.github.com>
2024-10-06 09:13:18 +0300

    mk go proto

---
+++ base/.local/share/scripts/mk.go
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+[ -f "$1" ] && exit 1
+
+echo '//usr/bin/go run $0 $@ ; exit $?' >> "$1"
+echo '// vim: set filetype=go:' >> "$1"
+echo '' >> "$1"
+echo 'package main' >> "$1"
+echo '' >> "$1"
+echo 'import "fmt"' >> "$1"
+echo '' >> "$1"
+echo 'func main() {' >> "$1"
+echo '  fmt.Println("Hello, world!")' >> "$1"
+echo '}' >> "$1"
+
+chmod +x "$1"
+
+[ "$2" = '-e' ] && nvim "$1"
+
+exit 0
