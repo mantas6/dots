046c8b7aad4c8664be6a50b0e232c6c6d29f9626
Mantas <6524518+mantas6@users.noreply.github.com>
2024-11-16 10:20:39 +0200

    Starting with flakes

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -11,6 +11,8 @@
       ./hardware-configuration.nix
     ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.timeout = 1;
