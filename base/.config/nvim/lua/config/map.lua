e76d5aebc7808f58013933a045eb962cd6f7b430
Mantas <6524518+mantas6@users.noreply.github.com>
2024-11-22 07:37:06 +0200

    add keybindings

--- base/.config/nvim/lua/config/map.lua
+++ base/.config/nvim/lua/config/map.lua
@@ -6,11 +6,19 @@ function ToggleHighlightSearch()
   end
 end
 
+function CopyCurrentBufferPath()
+  local filepath = vim.fn.expand('%:~:.')
+  vim.fn.setreg('+', filepath) -- Copy to system clipboard
+  vim.fn.setreg('"', filepath) -- Copy to unnamed register (default for pasting)
+  print('Copied buffer name: ' .. filepath)
+end
+
 -- Map <leader>h to toggle search highlighting
-vim.api.nvim_set_keymap('n', '<leader>h', ':lua ToggleHighlightSearch()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>bh', ':lua ToggleHighlightSearch()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>bf', ':LspZeroFormat<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>by', ':lua CopyCurrentBufferPath()<CR>', {})
 
 -- alias :W to :w
 vim.api.nvim_command('command W write')
 
 vim.api.nvim_command('command X set splitright | vsp | terminal bash %:p')
-
