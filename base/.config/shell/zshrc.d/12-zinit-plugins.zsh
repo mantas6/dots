fcb7977495ada13b047c307d7ca960df530ff713
Mantas <6524518+mantas6@users.noreply.github.com>
2024-11-24 16:51:02 +0200

    Use starship

--- base/.config/shell/zshrc.d/12-zinit-plugins.zsh
+++ base/.config/shell/zshrc.d/12-zinit-plugins.zsh
@@ -1,5 +1,3 @@
 #!/usr/bin/env zsh
 
-zinit ice depth=1; zinit light romkatv/powerlevel10k
-
 zinit light zsh-users/zsh-syntax-highlighting
--- base/.config/shell/zshrc.d/90-integrations.zsh
+++ base/.config/shell/zshrc.d/90-integrations.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+eval "$(starship init zsh)"
 eval "$(fzf --zsh)"
 eval "$(zoxide init zsh)"
 eval "$(direnv hook zsh)"
