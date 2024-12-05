5720fe5e6fab1bbce9cf09b987abf9ec6ae0e73e
mantas6 <6524518+mantas6@users.noreply.github.com>
2024-12-05 12:53:51 +0200

    Add dashboard start

--- base/.config/shell/zshrc.d/95-startx.zsh
+++ base/.config/shell/zshrc.d/95-startx.zsh
@@ -3,4 +3,8 @@
 if [ -x "$(command -v startx)" ] && [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; then
     echo "Start time: $(date)" >> "$XDG_CACHE_HOME/Xoutput"
     exec startx "$XINITRC" &>> "$XDG_CACHE_HOME/Xoutput"
+else
+    if [ "$(uname -n)" = 'l4' ]; then
+        dashb -f
+    fi
 fi
