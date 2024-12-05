cf11b78502f97e06a063f0ec03af68ce52c6922e
mantas6 <6524518+mantas6@users.noreply.github.com>
2024-12-05 13:06:36 +0200

    Update

--- base/.config/shell/zshrc.d/95-start.zsh
+++ base/.config/shell/zshrc.d/95-start.zsh
@@ -4,7 +4,7 @@ if [ -x "$(command -v startx)" ] && [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; 
     echo "Start time: $(date)" >> "$XDG_CACHE_HOME/Xoutput"
     exec startx "$XINITRC" &>> "$XDG_CACHE_HOME/Xoutput"
 else
-    if [ "$(uname -n)" = 'l4' ]; then
+    if [ "$(uname -n)" = 'l4' ] && [ "$XDG_VTNR" -eq 1 ]; then
         dashb -f
     fi
 fi
