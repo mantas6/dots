47c2d0a97bc415a153d9c3361cee7ab152576c2e
mantas6 <6524518+mantas6@users.noreply.github.com>
2024-12-18 14:29:11 +0200

    add vi mode plugin

--- base/.config/shell/zshrc.d/12-zinit-plugins.zsh
+++ base/.config/shell/zshrc.d/12-zinit-plugins.zsh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 
+zinit ice depth=1
+
 zinit light zsh-users/zsh-syntax-highlighting
 zinit light zsh-users/zsh-completions
 zinit light zsh-users/zsh-autosuggestions
 zinit light Aloxaf/fzf-tab
+zinit light jeffreytse/zsh-vi-mode
