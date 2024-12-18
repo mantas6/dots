0d5a8f1b79e97d970c651163b9518fd746dddd4d
mantas6 <6524518+mantas6@users.noreply.github.com>
2024-12-18 12:56:41 +0200

    toggl

---
+++ base/.local/share/services/packages/tg/app/Commands/Tasks/CopyCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Commands\Tasks;
+
+use App\Http\Integrations\Toggl\TogglConnector;
+use App\Project;
+use App\Task;
+use Illuminate\Contracts\Console\PromptsForMissingInput;
+use Illuminate\Process\Pipe;
+use Illuminate\Support\Facades\Process;
+use Illuminate\Support\Facades\Storage;
+use LaravelZero\Framework\Commands\Command;
+
+class CopyCommand extends Command implements PromptsForMissingInput
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tasks:cp {project-name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $tasks = Project::query()
+            ->where('name', 'like', '%' . $this->argument('project-name') . '%')
+            ->firstOrFail()
+            ->tasks
+            ->map(fn(Task $task) => $task->name);
+
+        Storage::put('tasks', $tasks->join("\n"));
+
+        $result = Process::pipe(function (Pipe $pipe) {
+            $pipe->command('cat ' . Storage::path('tasks'));
+            $pipe->command('fzf --tac');
+            $pipe->command('xc');
+        });
+    }
+}
