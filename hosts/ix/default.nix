d9a0fbb209e459d3bc2dd2aaa4dcd305dc04c1db
Mantas <6524518+mantas6@users.noreply.github.com>
2024-12-24 10:11:12 +0200

    Update

--- hosts/ix/default.nix
+++ hosts/ix/default.nix
@@ -1,4 +1,3 @@
-# vim: set tabstop=2 shiftwidth=2 expandtab:
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
