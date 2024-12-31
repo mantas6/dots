ac30986fe524ad44dc75286f3d887286a836932e
mantas6 <6524518+mantas6@users.noreply.github.com>
2024-12-31 13:07:43 +0200

    Update

--- base/.local/share/services/packages/zero/app/Commands/InspireCommand.php
+++ base/.local/share/services/packages/zero/app/Commands/InspireCommand.php
@@ -5,6 +5,8 @@ namespace App\Commands;
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Helper\TableCellStyle;
 use Symfony\Component\Console\Output\BufferedOutput;
 
 use function Mantas6\FzfPhp\fzf;
@@ -31,13 +33,15 @@ class InspireCommand extends Command
      */
     public function handle(): void
     {
-        $out = new BufferedOutput;
+        $out = new BufferedOutput(decorated: true);
         $table = new Table($out);
 
         $rows = [
-            [ 'title' => 'Oranges', 'id' => "\t1"],
-            [ 'title' => 'Apples', 'id' => "\t2"],
-            [ 'title' => 'Grapefruit', 'id' => "\t3"],
+            ['a' => '100', 'title' => "::Oranges", 'id' => "1", 'abc' => new TableCell('Testas', options: [
+                'style' => new TableCellStyle(['fg' => 'red'])
+            ])],
+            ['a' => '201', 'title' => "::Apples", 'id' => "2", 'abc' => ''],
+            ['a' => '300', 'title' => "::Grapefruit", 'id' => "3", 'abc' => ''],
         ];
 
         $table->setStyle('compact')
@@ -48,7 +52,11 @@ class InspireCommand extends Command
 
         $result = fzf(
             explode(PHP_EOL, $str),
-            ['d' => "\t", 'with-nth' => '1'],
+            [
+                'd' => "::",
+                'with-nth' => '2..',
+                'ansi' => true,
+            ],
         );
 
         dd($result);
