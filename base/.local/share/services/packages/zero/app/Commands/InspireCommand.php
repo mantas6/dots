ee4e405de7ebf557c5056903cbc07dfc5e8d0283
mantas6 <6524518+mantas6@users.noreply.github.com>
2024-12-31 12:09:19 +0200

    Update

--- base/.local/share/services/packages/zero/app/Commands/InspireCommand.php
+++ base/.local/share/services/packages/zero/app/Commands/InspireCommand.php
@@ -14,7 +14,7 @@ class InspireCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'inspire {name=Artisan}';
+    protected $signature = 'inspire';
 
     /**
      * The description of the command.
@@ -28,21 +28,6 @@ class InspireCommand extends Command
      */
     public function handle(): void
     {
-        render(<<<'HTML'
-            <div class="py-1 ml-2">
-                <div class="px-1 bg-blue-300 text-black">Laravel Zero</div>
-                <em class="ml-1">
-                  Simplicity is the ultimate sophistication.
-                </em>
-            </div>
-        HTML);
-    }
 
-    /**
-     * Define the command's schedule.
-     */
-    public function schedule(Schedule $schedule): void
-    {
-        // $schedule->command(static::class)->everyMinute();
     }
 }
