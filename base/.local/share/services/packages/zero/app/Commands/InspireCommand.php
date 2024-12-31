891be68669f655d4c6956dd8a95eb9e6e4147666
mantas6 <6524518+mantas6@users.noreply.github.com>
2024-12-31 12:19:22 +0200

    Update

--- base/.local/share/services/packages/zero/app/Commands/InspireCommand.php
+++ base/.local/share/services/packages/zero/app/Commands/InspireCommand.php
@@ -4,7 +4,10 @@ namespace App\Commands;
 
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Output\BufferedOutput;
 
+use function Mantas6\FzfPhp\fzf;
 use function Termwind\render;
 
 class InspireCommand extends Command
@@ -28,6 +31,26 @@ class InspireCommand extends Command
      */
     public function handle(): void
     {
+        $out = new BufferedOutput;
+        $table = new Table($out);
 
+        $rows = [
+            [ 'title' => 'Oranges', 'id' => "\t1"],
+            [ 'title' => 'Apples', 'id' => "\t2"],
+            [ 'title' => 'Grapefruit', 'id' => "\t3"],
+        ];
+
+        $table->setStyle('compact')
+            ->setRows($rows)
+            ->render();
+
+        $str = $out->fetch();
+
+        $result = fzf(
+            explode(PHP_EOL, $str),
+            ['d' => "\t", 'with-nth' => '1'],
+        );
+
+        dd($result);
     }
 }
