78a7393af7ecc88e2c1800f6d632b8873e5165f7
Mantas <6524518+mantas6@users.noreply.github.com>
2025-01-11 20:49:44 +0200

    Update

--- base/.config/awesome/apps.lua
+++ base/.config/awesome/apps.lua
@@ -25,7 +25,7 @@ return function()
       exec = {
         'chromium --new-window https://www.youtube.com/feed/subscriptions',
         'chromium --new-window https://www.netflix.com/browse',
-        'chromium https://discord.com',
+        'chromium https://discord.com/channels/@me',
       }
     },
 --     {
