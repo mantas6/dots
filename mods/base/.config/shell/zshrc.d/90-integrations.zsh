8595886af15d9ea4b76dff938a7bc9930bf17b14
mantas6 <6524518+mantas6@users.noreply.github.com>
2025-01-14 11:03:51 +0200

    fix zi

--- mods/base/.config/shell/zshrc.d/90-integrations.zsh
+++ mods/base/.config/shell/zshrc.d/90-integrations.zsh
@@ -2,7 +2,10 @@
 
 [ -x "$(command -v starship)" ] && eval "$(starship init zsh)"
 [ -x "$(command -v fzf)" ] && eval "$(fzf --zsh)"
+
+unalias zi
 [ -x "$(command -v zoxide)" ] && eval "$(zoxide init zsh)"
+
 [ -x "$(command -v direnv)" ] && eval "$(direnv hook zsh)"
 
 [ -f "$XDG_STATE_HOME/.inhibit-suspend" ] && gum log --level info 'Suspend is inhibited'
