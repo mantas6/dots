aa04b4254ef51247737d1085f14479252d8cde39
Mantas <6524518+mantas6@users.noreply.github.com>
2025-03-16 09:20:35 +0200

    add transient experimental prompt

--- mods/base/.config/shell/zshrc.d/90-integrations.zsh
+++ mods/base/.config/shell/zshrc.d/90-integrations.zsh
@@ -1,6 +1,24 @@
 #!/usr/bin/env zsh
 
-[ -x "$(command -v starship)" ] && eval "$(starship init zsh)"
+if [ -x "$(command -v starship)" ]; then
+    eval "$(starship init zsh)"
+
+    set-long-prompt() { PROMPT=$(starship prompt) }
+    precmd_functions=(set-long-prompt)
+
+    set-short-prompt() {
+      if [[ $PROMPT != '%# ' ]]; then
+        PROMPT="$(starship prompt --profile transient)"
+        zle .reset-prompt 2>/dev/null # hide the errors on ctrl+c
+      fi
+    }
+
+    zle-line-finish() { set-short-prompt }
+    zle -N zle-line-finish
+
+    trap 'set-short-prompt; return 130' INT
+fi
+
 [ -x "$(command -v fzf)" ] && eval "$(fzf --zsh)"
 
 unalias zi
