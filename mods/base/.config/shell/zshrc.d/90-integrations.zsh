19e3e90f38d306e9d3425b4ac4b24cab96dbd075
mantas6 <6524518+mantas6@users.noreply.github.com>
2025-03-17 09:39:33 +0200

    remove transient prompt since it breaks the autosuggetions

--- mods/base/.config/shell/zshrc.d/90-integrations.zsh
+++ mods/base/.config/shell/zshrc.d/90-integrations.zsh
@@ -1,24 +1,6 @@
 #!/usr/bin/env zsh
 
-if [ -x "$(command -v starship)" ]; then
-    eval "$(starship init zsh)"
-
-    set-long-prompt() { PROMPT=$(starship prompt) }
-    precmd_functions=(set-long-prompt)
-
-    set-short-prompt() {
-      if [[ $PROMPT != '%# ' ]]; then
-        PROMPT="$(starship prompt --profile transient)"
-        zle .reset-prompt 2>/dev/null # hide the errors on ctrl+c
-      fi
-    }
-
-    zle-line-finish() { set-short-prompt }
-    zle -N zle-line-finish
-
-    trap 'set-short-prompt; return 130' INT
-fi
-
+[ -x "$(command -v starship)" ] && eval "$(starship init zsh)"
 [ -x "$(command -v fzf)" ] && eval "$(fzf --zsh)"
 
 unalias zi
