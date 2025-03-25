d46e4317e02901e634d959ebba389db5631586e3
mantas6 <6524518+mantas6@users.noreply.github.com>
2025-03-25 09:21:09 +0200

    remove markdown preview plugin

--- mods/base/.config/nvim/lua/plugins/markdown.lua
+++ mods/base/.config/nvim/lua/plugins/markdown.lua
@@ -8,16 +8,4 @@ return {
 
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
   },
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-    keys = {
-      { '<leader>mp', ':MarkdownPreviewToggle<CR>' },
-    },
-  },
 }
