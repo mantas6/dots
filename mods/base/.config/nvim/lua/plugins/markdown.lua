1d4551a604545d6688d2fcb8db9f02323c593fb7
mantas6 <6524518+mantas6@users.noreply.github.com>
2025-04-23 11:11:20 +0300

    improve markdown rendering when code blocks are used

--- mods/base/.config/nvim/lua/plugins/markdown.lua
+++ mods/base/.config/nvim/lua/plugins/markdown.lua
@@ -3,6 +3,14 @@ return {
     'MeanderingProgrammer/render-markdown.nvim',
 
     opts = {
+      code = {
+        -- Determines how the top / bottom of code block are rendered.
+        -- | none  | do not render a border                               |
+        -- | thick | use the same highlight as the code body              |
+        -- | thin  | when lines are empty overlay the above & below icons |
+        -- | hide  | conceal lines unless language name or icon is added  |
+        border = 'thin',
+      },
       heading = { enabled = false },
     },
 
