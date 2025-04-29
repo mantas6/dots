cf58a11528ce9fa19e9da2c2cf974d444540938a
Mantas <6524518+mantas6@users.noreply.github.com>
2025-04-29 10:49:12 +0300

    update key, bm fix

--- nix/modules/user/authorized-keys.nix
+++ nix/modules/user/authorized-keys.nix
@@ -2,6 +2,6 @@
   users.users.mantas.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtMJ6SP+1ppYvlbRSDyjhmWvDFOvKGFMD7V88h7Q6Ni mantas@amd"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKwmj+D1NO4kg3E6JH4ck0q+C65hTiTh69POfqXMROhF mantas@X13"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILjrGnrqRPmgmoYPHRechhllrxeWXZ5gpAbDch3IJVRZ mantas@Mantass-MBP.lan"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC5k7rbC+vefo20If2RHDFMxztdC6tkeUaN88tentEeh mantas@MacBookPro.lan"
   ];
 }
