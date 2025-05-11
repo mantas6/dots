d9a70b0bb01ebb037f1691da2aa104b8c963adb7
Mantas <6524518+mantas6@users.noreply.github.com>
2025-05-11 07:17:11 +0300

    zero sync composer, toggl interation initial

---
+++ lib/zero/app/Commands/TimeEntries/StartCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Commands\TimeEntries;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class StartCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'time:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+
+    /**
+     * Define the command's schedule.
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
---
+++ lib/zero/app/TimeEntry.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TimeEntry extends Model
+{
+    use HasFactory;
+}
