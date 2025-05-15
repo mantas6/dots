a27f23aa75ef2e77cf543f419f1ebbed61c04c99
mantas6 <6524518+mantas6@users.noreply.github.com>
2025-05-15 13:17:26 +0300

    add base qutebrowser cfg

--- mods/base/.config/qutebrowser/config.py
+++ mods/base/.config/qutebrowser/config.py
@@ -1,5 +1,11 @@
+# pylint: disable=C0111
+c = c  # noqa: F821 pylint: disable=E0602,C0103
+config = config  # noqa: F821 pylint: disable=E0602,C0103
+
+config.load_autoconfig()
+
 c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
 c.colors.webpage.darkmode.policy.images = 'never'
 
-c.fonts.default_size = '21pt'
+c.fonts.default_size = '16pt'
