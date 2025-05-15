f5402db648344a31cd63adb3fd2128ae3a773f05
mantas6 <6524518+mantas6@users.noreply.github.com>
2025-05-15 12:57:31 +0300

    add base qutebrowser cfg

---
+++ mods/base/.config/qutebrowser/config.py
@@ -0,0 +1,5 @@
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
+c.colors.webpage.darkmode.policy.images = 'never'
+
+c.fonts.default_size = '21pt'
