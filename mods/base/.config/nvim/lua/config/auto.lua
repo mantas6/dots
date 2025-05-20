255e48df5b3585a0de231517ba21d29db763c494
mantas6 <6524518+mantas6@users.noreply.github.com>
2025-05-20 11:27:15 +0300

    high light when yanking

--- mods/base/.config/nvim/lua/config/auto.lua
+++ mods/base/.config/nvim/lua/config/auto.lua
@@ -2,6 +2,18 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 local group = augroup('MyAutoCommands', { clear = true })
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
 
 autocmd('BufEnter', {
     group = group,
