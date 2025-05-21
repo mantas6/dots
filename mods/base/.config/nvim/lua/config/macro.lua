2f4ff44da0ab6d02e6866698d1331b047f5da56d
mantas6 <6524518+mantas6@users.noreply.github.com>
2025-05-21 09:51:53 +0300

    setreg for macros autocmd

--- mods/base/.config/nvim/lua/config/macro.lua
+++ mods/base/.config/nvim/lua/config/macro.lua
@@ -1,2 +1,15 @@
-vim.keymap.set("n", "<leader>ml", "oconsole.log()<Esc>ha")
-vim.keymap.set("n", "<leader>mo", "$i<Enter><Esc>O")
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local group = augroup('MyMacroCommands', { clear = true })
+
+autocmd('BufEnter', {
+  group = group,
+  pattern = '*',
+  callback = function()
+    vim.fn.setreg('l',  "oconsole.log()ha")
+  end
+})
+
+-- vim.keymap.set("n", "<leader>ml", "oconsole.log()<Esc>ha")
+-- vim.keymap.set("n", "<leader>mo", "$i<Enter><Esc>O")
