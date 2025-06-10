2573b92bdf91c092d098d5cdf269da337bb14f91
mantas6 <6524518+mantas6@users.noreply.github.com>
2025-06-10 11:37:09 +0300

    add blame, f

--- mods/base/.config/nvim/lua/plugins/git.lua
+++ mods/base/.config/nvim/lua/plugins/git.lua
@@ -1,7 +1,7 @@
 return {
   "lewis6991/gitsigns.nvim",
 
-  config = function ()
+  config = function()
     require('gitsigns').setup({
       signs = {
         add = { text = '+' },
@@ -25,14 +25,14 @@ return {
           if vim.wo.diff then return ']c' end
           vim.schedule(function() gs.next_hunk() end)
           return '<Ignore>'
-        end, {expr=true})
+        end, { expr = true })
 
         -- Navigation through hunks
         map('n', '[c', function()
           if vim.wo.diff then return '[c' end
           vim.schedule(function() gs.prev_hunk() end)
           return '<Ignore>'
-        end, {expr=true})
+        end, { expr = true })
 
         -- Actions
         map('n', '<leader>gS', gs.stage_buffer)
@@ -42,6 +42,12 @@ return {
         map('n', '<leader>ge', gs.reset_buffer)
         map('n', '<leader>gk', gs.preview_hunk)
         map('n', '<leader>gv', ':<C-U>Gitsigns select_hunk<CR>')
+
+        map('n', '<leader>gB', function()
+          gs.blame_line({ full = true })
+        end)
+
+        map('n', '<leader>gb', gs.blame_line)
 
         map('n', '<leader>gl', gs.diffthis)
         -- map('n', '<leader>gD', function() gs.diffthis('~') end)
