5b8562356919248ad3bdce2f8e4d18d51f42550b
Mantas <6524518+mantas6@users.noreply.github.com>
2025-06-26 07:59:41 +0300

    add l4 host

---
+++ nix/hosts/l4/default.nix
@@ -0,0 +1,35 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+{...}: {
+  imports = [
+    ./hardware.nix
+    ../../modules
+  ];
+
+  disko.devices.disk.main-disk.device = "/dev/sda";
+
+  features.list = [
+    "disks/normal"
+  ];
+
+  systemd.sleep.extraConfig = ''
+    AllowSuspend=no
+    AllowHibernation=no
+    AllowHybridSleep=no
+    AllowSuspendThenHibernate=no
+  '';
+
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 8 * 1024;
+    }
+  ];
+
+  console.font = "ter-120b";
+
+  networking.hostName = "l4";
+
+  system.stateVersion = "25.05";
+}
