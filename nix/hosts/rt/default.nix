f5a248df6a2a8cb9b85c9ceb6bb4c14880c24aff
Mantas <6524518+mantas6@users.noreply.github.com>
2025-08-10 16:40:12 +0300

    enable serial console on all devs

--- nix/hosts/rt/default.nix
+++ nix/hosts/rt/default.nix
@@ -18,6 +18,8 @@
   boot.kernelParams = [
     "console=ttyS0,115200n8"
     "console=ttyS1,115200n8"
+    "console=ttyS2,115200n8"
+    "console=ttyS3,115200n8"
   ];
   boot.loader.grub.extraConfig = "
    serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1
