13a5a9ae006c7c6315047c83eb94ea87cd8a080c
Mantas <6524518+mantas6@users.noreply.github.com>
2025-09-16 09:04:29 +0300

    nvm setup

--- sh/zprofile.d/60-darwin.zsh
+++ sh/zprofile.d/60-darwin.zsh
@@ -21,4 +21,8 @@ fi
 [ -d "$HOME/Applications/Docker.app/Contents/Resources/bin" ] && \
     export PATH="$HOME/Applications/Docker.app/Contents/Resources/bin:$PATH"
 
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 export DOCKER_CLI_HINTS=false
