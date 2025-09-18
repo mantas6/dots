f90f7f99a513aa22dc76c8b974177d6a29babf31
Mantas <6524518+mantas6@users.noreply.github.com>
2025-09-18 08:56:33 +0300

    add printer manually

--- nix/modules/features/printing.nix
+++ nix/modules/features/printing.nix
@@ -10,13 +10,31 @@ in {
     {features.listAvailable = [name];}
     (lib.mkIf (lib.elem name config.features.list) {
       services = {
-        printing.enable = true;
+        printing = {
+          enable = true;
+          drivers = with pkgs; [
+            cups-filters
+            cups-browsed
+          ];
+        };
 
         avahi = {
           enable = true;
           nssmdns4 = true;
           openFirewall = true;
         };
+      };
+
+      hardware.printers = {
+        ensureDefaultPrinter = "Brother_HL-L2445DW";
+        ensurePrinters = [
+          {
+            deviceUri = "ipp://BRN94DDF82B172F/ipp";
+            location = "home";
+            name = "Brother_HL-L2445DW";
+            model = "everywhere";
+          }
+        ];
       };
     })
   ];
