723f43767a9faaa30360570e3fd2f51381791d89
Mantas <6524518+mantas6@users.noreply.github.com>
2025-09-18 09:24:16 +0300

    initial wez config

---
+++ mods/base/.config/wezterm/wezterm.lua
@@ -0,0 +1,33 @@
+local w = require 'wezterm'
+
+local c = w.config_builder()
+local a = w.action
+
+c.disable_default_key_bindings = true
+c.disable_default_mouse_bindings = true
+c.enable_tab_bar = false
+
+c.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+c.font_size = 16
+c.color_scheme = 'Tokyo Night'
+
+c.keys = {
+  {
+    key = 'c',
+    mods = 'SUPER',
+    action = a.CopyTo 'Clipboard'
+  },
+  {
+    key = 'v',
+    mods = 'SUPER',
+    action = a.PasteFrom 'Clipboard'
+  },
+}
+
+return c
