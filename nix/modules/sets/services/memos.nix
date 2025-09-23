c5c9e4cda3cec31d311e2811ee2bba6da8e5a6d1
Mantas <6524518+mantas6@users.noreply.github.com>
2025-09-23 08:50:52 +0300

    memos version pin

--- nix/modules/sets/services/memos.nix
+++ nix/modules/sets/services/memos.nix
@@ -13,7 +13,7 @@ in {
 
       virtualisation.oci-containers.containers = {
         memos = {
-          image = "neosmemo/memos:stable";
+          image = "neosmemo/memos:0.25.1";
           user = "1000";
           ports = ["0.0.0.0:5230:5230"];
           volumes = [
