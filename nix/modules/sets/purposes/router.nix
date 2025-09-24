12991e8fe6369a798e4ca00a10e37fb02f018c8e
Mantas <6524518+mantas6@users.noreply.github.com>
2025-09-24 08:35:44 +0300

    router config

--- nix/modules/sets/purposes/router.nix
+++ nix/modules/sets/purposes/router.nix
@@ -5,11 +5,11 @@
 }: let
   name = "purposes.router";
 
-  lanDeviceName = "net0";
-  lanDeviceMac = "abc";
+  lanIfName = "net0";
+  lanDeviceMac = "98:fa:9b:9e:ef:fb";
 
-  wanDeviceName = "internet0";
-  wanDeviceMac = "abc";
+  wanIfName = "internet0";
+  wanDeviceMac = "00:1b:21:f0:6c:e0";
 in {
   config = lib.mkMerge [
     {features.setsAvailable = [name];}
@@ -26,7 +26,7 @@ in {
             "1.1.1.1"
           ];
 
-          # interface="net0";
+          # interface="${lanIfName}";
 
           # listenAddress="::1,127.0.0.1,192.168.0.1";
           # dhcpRange="192.168.0.1,192.168.0.255,infinite";
@@ -41,17 +41,17 @@ in {
         };
       };
 
-      systemd.network.links."10-${lanDeviceName}" = {
+      systemd.network.links."10-${lanIfName}" = {
         matchConfig.PermanentMACAddress = lanDeviceMac;
         linkConfig = {
-          Name = lanDeviceName;
+          Name = lanIfName;
         };
       };
 
-      systemd.network.links."10-${wanDeviceName}" = {
+      systemd.network.links."10-${wanIfName}" = {
         matchConfig.PermanentMACAddress = wanDeviceMac;
         linkConfig = {
-          Name = wanDeviceName;
+          Name = wanIfName;
         };
       };
 
@@ -61,11 +61,80 @@ in {
         "net.ipv6.conf.all.forwarding" = 1;
       };
 
+      networking = {
+        nat.enable = false;
+        firewall.enable = false;
+
+        nftables = {
+          enable = true;
+
+          # ruleset = ''
+          #   table inet filter {
+          #     # enable flow offloading for better throughput
+          #     flowtable f {
+          #       hook ingress priority 0;
+          #       devices = { ppp0, lan };
+          #     }
+          #
+          #     chain output {
+          #       type filter hook output priority 100; policy accept;
+          #     }
+          #
+          #     chain input {
+          #       type filter hook input priority filter; policy drop;
+          #
+          #       # Allow trusted networks to access the router
+          #       iifname {
+          #         "lan",
+          #       } counter accept
+          #
+          #       # Allow returning traffic from ppp0 and drop everthing else
+          #       iifname "ppp0" ct state { established, related } counter accept
+          #       iifname "ppp0" drop
+          #     }
+          #
+          #     chain forward {
+          #       type filter hook forward priority filter; policy drop;
+          #
+          #       # enable flow offloading for better throughput
+          #       ip protocol { tcp, udp } flow offload @f
+          #
+          #       # Allow trusted network WAN access
+          #       iifname {
+          #               "lan",
+          #       } oifname {
+          #               "ppp0",
+          #       } counter accept comment "Allow trusted LAN to WAN"
+          #
+          #       # Allow established WAN to return
+          #       iifname {
+          #               "ppp0",
+          #       } oifname {
+          #               "lan",
+          #       } ct state established,related counter accept comment "Allow established back to LANs"
+          #     }
+          #   }
+          #
+          #   table ip nat {
+          #     chain prerouting {
+          #       type nat hook prerouting priority filter; policy accept;
+          #     }
+          #
+          #     # Setup NAT masquerading on the ppp0 interface
+          #     chain postrouting {
+          #       type nat hook postrouting priority filter; policy accept;
+          #       oifname "ppp0" masquerade
+          #     }
+          #   }
+          # '';
+        };
+      };
+
       # networking.firewall.extraCommands = [
-      #   "iptables -t nat -A POSTROUTING -o ${wanDeviceName} -j MASQUERADE"
+      #   "iptables -t nat -A POSTROUTING -o ${wanIfName} -j MASQUERADE"
       #   "iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT"
-      #   "iptables -A FORWARD -i ${lanDeviceName} -o ${wanDeviceName} -j ACCEPT"
-      #   "iptables -I INPUT -p udp --dport 67 -i ${lanDeviceName}  -j ACCEPT"
+      #   "iptables -A FORWARD -i ${lanIfName} -o ${wanIfName} -j ACCEPT"
+      #   "iptables -I INPUT -p udp --dport 67 -i ${lanIfName}  -j ACCEPT"
       #   "iptables -I INPUT -p udp --dport 53 -s 192.168.123.0/24 -j ACCEPT"
       #   "iptables -I INPUT -p tcp --dport 53 -s 192.168.123.0/24 -j ACCEPT"
       # ];
