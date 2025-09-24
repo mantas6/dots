451277a83dbd090d6ab68fe59c0e2d483cd2c631
Mantas <6524518+mantas6@users.noreply.github.com>
2025-09-24 09:50:27 +0300

    fix

--- nix/modules/sets/services/memos.nix
+++ nix/modules/sets/services/memos.nix
@@ -11,7 +11,7 @@ in {
       virtualisation.oci-containers.containers = {
         memos = {
           # image = "neosmemo/memos:0.25.1";
-          image = "neosmemo/memos:latest";
+          image = "neosmemo/memos:stable";
           user = "1000";
           ports = ["0.0.0.0:5230:5230"];
           volumes = [
