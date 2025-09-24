27b90da55e9ec76e59671be83e40edfe4a863e69
Mantas <6524518+mantas6@users.noreply.github.com>
2025-09-24 09:25:07 +0300

    bell for failed commands

--- sh/zshrc.d/50-opts.zsh
+++ sh/zshrc.d/50-opts.zsh
@@ -39,3 +39,9 @@ zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 zstyle ':completion:*' menu no
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
 zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
+precmd() {
+  if [[ $? -ne 0 ]]; then
+    print -n '\a'
+  fi
+}
