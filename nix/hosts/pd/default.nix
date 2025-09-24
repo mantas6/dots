a2f8b96309d2e1c57fbd2349205177c701f36413
Mantas <6524518+mantas6@users.noreply.github.com>
2025-09-24 08:42:42 +0300

    add root key

--- nix/hosts/pd/default.nix
+++ nix/hosts/pd/default.nix
@@ -15,6 +15,10 @@
     "purposes.router"
   ];
 
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJO2Vhr1xayCvuo+GT3lP3VCcpgaIR4z6TlFGLwex7yv mantas@ix"
+  ];
+
   networking.hostName = "pd";
 
   system.stateVersion = "25.05";
