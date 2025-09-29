b0fcc4b4b64d5485dc7ed970080e812d1eab7fce
Mantas <6524518+mantas6@users.noreply.github.com>
2025-09-29 08:46:31 +0300

    router config progress

--- nix/modules/sets/purposes/router.nix
+++ nix/modules/sets/purposes/router.nix
@@ -7,6 +7,8 @@
 
   lanIfName = "net0";
   lanDeviceMac = "98:fa:9b:9e:ef:fb";
+  lanIp = "10.0.1.1";
+  lanIpRangeEnd = "10.0.1.255";
 
   wanIfName = "internet0";
   wanDeviceMac = "00:1b:21:f0:6c:e0";
@@ -26,10 +28,10 @@ in {
             "1.1.1.1"
           ];
 
-          # interface="${lanIfName}";
+          interface = "${lanIfName}";
 
-          # listenAddress="::1,127.0.0.1,192.168.0.1";
-          # dhcpRange="192.168.0.1,192.168.0.255,infinite";
+          listenAddress = "::1,127.0.0.1,${lanIp}";
+          dhcpRange = "${lanIp},${lanIpRangeEnd},infinite";
 
           cache-size = 10000;
 
@@ -56,88 +58,91 @@ in {
       };
 
       boot.kernel.sysctl = {
-        "net.ipv4.ip_forward" = 1;
-        "net.ipv4.conf.all.forwarding" = 1;
-        "net.ipv6.conf.all.forwarding" = 1;
+        "net.ipv4.ip_forward" = true;
+        "net.ipv4.conf.all.forwarding" = true;
+        "net.ipv6.conf.all.forwarding" = true;
       };
 
       networking = {
+        interfaces = {
+          "${lanIfName}" = {
+            useDHCP = false;
+            ipv4.addresses = [
+              {
+                address = "${lanIp}";
+                prefixLength = 24;
+              }
+            ];
+          };
+        };
+
         nat.enable = false;
         firewall.enable = false;
 
         nftables = {
           enable = true;
 
-          # ruleset = ''
-          #   table inet filter {
-          #     # enable flow offloading for better throughput
-          #     flowtable f {
-          #       hook ingress priority 0;
-          #       devices = { ppp0, lan };
-          #     }
-          #
-          #     chain output {
-          #       type filter hook output priority 100; policy accept;
-          #     }
-          #
-          #     chain input {
-          #       type filter hook input priority filter; policy drop;
-          #
-          #       # Allow trusted networks to access the router
-          #       iifname {
-          #         "lan",
-          #       } counter accept
-          #
-          #       # Allow returning traffic from ppp0 and drop everthing else
-          #       iifname "ppp0" ct state { established, related } counter accept
-          #       iifname "ppp0" drop
-          #     }
-          #
-          #     chain forward {
-          #       type filter hook forward priority filter; policy drop;
-          #
-          #       # enable flow offloading for better throughput
-          #       ip protocol { tcp, udp } flow offload @f
-          #
-          #       # Allow trusted network WAN access
-          #       iifname {
-          #               "lan",
-          #       } oifname {
-          #               "ppp0",
-          #       } counter accept comment "Allow trusted LAN to WAN"
-          #
-          #       # Allow established WAN to return
-          #       iifname {
-          #               "ppp0",
-          #       } oifname {
-          #               "lan",
-          #       } ct state established,related counter accept comment "Allow established back to LANs"
-          #     }
-          #   }
-          #
-          #   table ip nat {
-          #     chain prerouting {
-          #       type nat hook prerouting priority filter; policy accept;
-          #     }
-          #
-          #     # Setup NAT masquerading on the ppp0 interface
-          #     chain postrouting {
-          #       type nat hook postrouting priority filter; policy accept;
-          #       oifname "ppp0" masquerade
-          #     }
-          #   }
-          # '';
+          ruleset = ''
+            table inet filter {
+              # enable flow offloading for better throughput
+              flowtable f {
+                hook ingress priority 0;
+                devices = { ${wanIfName}, ${lanIfName} };
+              }
+
+              chain output {
+                type filter hook output priority 100; policy accept;
+              }
+
+              chain input {
+                type filter hook input priority filter; policy drop;
+
+                # Allow trusted networks to access the router
+                iifname {
+                  "${lanIfName}",
+                } counter accept
+
+                # Allow returning traffic from ${wanIfName} and drop everthing else
+                iifname "${wanIfName}" ct state { established, related } counter accept
+                iifname "${wanIfName}" drop
+              }
+
+              chain forward {
+                type filter hook forward priority filter; policy drop;
+
+                # enable flow offloading for better throughput
+                ip protocol { tcp, udp } flow offload @f
+
+                # Allow trusted network WAN access
+                iifname {
+                        "${lanIfName}",
+                } oifname {
+                        "${wanIfName}",
+                } counter accept comment "Allow trusted LAN to WAN"
+
+                # Allow established WAN to return
+                iifname {
+                        "${wanIfName}",
+                } oifname {
+                        "${lanIfName}",
+                } ct state established,related counter accept comment "Allow established back to LANs"
+              }
+            }
+
+            table ip nat {
+              chain prerouting {
+                type nat hook prerouting priority filter; policy accept;
+              }
+
+              # Setup NAT masquerading on the ${wanIfName} interface
+              chain postrouting {
+                type nat hook postrouting priority filter; policy accept;
+                oifname "${wanIfName}" masquerade
+              }
+            }
+          '';
         };
       };
-
-      # networking.firewall.extraCommands = [
-      #   "iptables -t nat -A POSTROUTING -o ${wanIfName} -j MASQUERADE"
-      #   "iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT"
-      #   "iptables -A FORWARD -i ${lanIfName} -o ${wanIfName} -j ACCEPT"
-      #   "iptables -I INPUT -p udp --dport 67 -i ${lanIfName}  -j ACCEPT"
-      #   "iptables -I INPUT -p udp --dport 53 -s 192.168.123.0/24 -j ACCEPT"
-      #   "iptables -I INPUT -p tcp --dport 53 -s 192.168.123.0/24 -j ACCEPT"
-      # ];
     })
   ];
 }
