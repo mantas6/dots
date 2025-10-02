26878b11364d6fa49584ec4f4f5580a1d2db512c
Mantas <6524518+mantas6@users.noreply.github.com>
2025-10-02 11:21:55 +0300

    update tin, attempt to print all glob vars

--- etc/stubs/tinker-index.php
+++ etc/stubs/tinker-index.php
@@ -7,12 +7,25 @@ $app = require_once __DIR__.'/bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 
 $status = $kernel->handle(
-   $input = new Symfony\Component\Console\Input\ArgvInput,
+    $input = new Symfony\Component\Console\Input\ArgvInput,
     new Symfony\Component\Console\Output\BufferedOutput
 );
+
+$result = null;
+$definedVariables = [];
+$definedVariables = array_keys(get_defined_vars());
 
 $result = require './.tinker-buffer.php';
 
 dump($result);
+echo "\n";
+
+$variables = array_filter(
+    array: get_defined_vars(),
+    callback: fn ($key) => !in_array($key, $definedVariables),
+    mode: ARRAY_FILTER_USE_KEY
+);
+
+dump($variables);
 
 $kernel->terminate($input, $status);
