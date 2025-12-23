941647242594372c65d7bb711936c192aafe09f1
Mantas <6524518+mantas6@users.noreply.github.com>
2025-12-23 09:26:45 +0200

    battery

--- lib/bar/main.go
+++ lib/bar/main.go
@@ -32,6 +32,11 @@ type UptimeResult struct {
 	Uptime int64 `json:"uptime"`
 }
 
+type BatteryResult struct {
+	Capacity float64 `json:"capacity"`
+	Status   string  `json:"status"`
+}
+
 func cpuUsage(parts *[]string, res json.RawMessage) {
 	var usages []float64
 	if err := json.Unmarshal(res, &usages); err != nil {
@@ -90,16 +95,15 @@ func uptime(parts *[]string, res json.RawMessage) {
 	time := p.Uptime / 1000 / 60
 	unit := "m"
 
-	if (time > 60) {
+	if time > 60 {
 		time /= 60
 		unit = "h"
 
-		if (time > 24) {
+		if time > 24 {
 			time /= 24
 			unit = "d"
 		}
 	}
-
 
 	*parts = append(*parts, fmt.Sprintf("󰐦 %v%s", time, unit))
 }
@@ -107,6 +111,37 @@ func uptime(parts *[]string, res json.RawMessage) {
 func clock(parts *[]string) {
 	s := time.Now().Format(time.TimeOnly)
 	*parts = append(*parts, fmt.Sprintf(" %v", s))
+}
+
+func battery(parts *[]string, res json.RawMessage) {
+	var p BatteryResult
+
+	if err := json.Unmarshal(res, &p); err != nil {
+		return
+	}
+
+	icon := ""
+
+	switch p.Status {
+	case "AC Connected":
+		icon = ""
+	case "Charging":
+		icon = "󱐋"
+	default:
+		if p.Capacity > 80 {
+			icon = ""
+		} else if p.Capacity > 60 {
+			icon = ""
+		} else if p.Capacity > 40 {
+			icon = ""
+		} else if p.Capacity > 20 {
+			icon = ""
+		} else {
+			icon = ""
+		}
+	}
+
+	*parts = append(*parts, fmt.Sprintf("%v %.0f", icon, p.Capacity))
 }
 
 func main() {
@@ -146,6 +181,8 @@ func main() {
 			kernelVersion(parts, res)
 		case "Uptime":
 			uptime(parts, res)
+		case "Battery":
+			battery(parts, res)
 		}
 	}
 
