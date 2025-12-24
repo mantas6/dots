aba7e2dae4b3e3e542f26c8086141fe0e00ee028
Mantas <6524518+mantas6@users.noreply.github.com>
2025-12-24 09:23:21 +0200

    move across files

---
+++ lib/bar/progs.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+func getFastfetchData() (metrics []Metric) {
+	cmd := exec.Command(
+		"fastfetch",
+		"--format", "json",
+		"-c",
+		"bar.jsonc",
+	)
+
+	out, err := cmd.Output()
+
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(out, &metrics)
+
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func getPingInterval() string {
+	cmd := exec.Command("ping", "-c", "1", "-W", "1", "google.com")
+
+	out, err := cmd.Output()
+
+	if err != nil {
+		return "-"
+	}
+
+	re := regexp.MustCompile(`time=([\d.]+) ms`)
+	match := re.FindStringSubmatch(string(out))
+
+	if len(match) < 2 {
+		return "-"
+	}
+
+	return strings.Split(match[1], ".")[0] + "ms"
+}
---
+++ lib/bar/types.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+)
+
+type Metric struct {
+	Type   string          `json:"type"`
+	Result json.RawMessage `json:"result"`
+}
+
+type KernelResult struct {
+	Release string `json:"release"`
+}
+
+type MemoryResult struct {
+	Total int64 `json:"total"`
+	Used  int64 `json:"used"`
+}
+
+type CpuResult struct {
+	Temperature float64 `json:"temperature"`
+}
+
+type UptimeResult struct {
+	Uptime int64 `json:"uptime"`
+}
+
+type BatteryResult struct {
+	Capacity float64 `json:"capacity"`
+	Status   string  `json:"status"`
+}
+
+type DiskBytesResult struct {
+	Used  int64 `json:"used"`
+	Total int64 `json:"total"`
+}
+
+type DiskResult struct {
+	Mountpoint string          `json:"mountpoint"`
+	Bytes      DiskBytesResult `json:"bytes"`
+}
