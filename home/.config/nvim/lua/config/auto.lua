b5c7efa94276b2a615be299e599ce5f4c5ec6daa
Mantas <6524518+mantas6@users.noreply.github.com>
2026-01-06 11:40:33 +0200

    set tabwith for jsx/tsx

--- home/.config/nvim/lua/config/auto.lua
+++ home/.config/nvim/lua/config/auto.lua
@@ -5,31 +5,43 @@ local group = augroup('MyAutoCommands', { clear = true })
 local yank_group = augroup('HighlightYank', {})
 
 autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.hl.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+  group = yank_group,
+  pattern = '*',
+  callback = function()
+    vim.hl.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40,
+    })
+  end,
 })
 
 autocmd('BufEnter', {
-    group = group,
-    callback = function()
-        vim.opt.autoindent = true
-        vim.opt.smartindent = true
-    end
+  group = group,
+  callback = function()
+    vim.opt.autoindent = true
+    vim.opt.smartindent = true
+  end
 })
 
 autocmd('FileType', {
-    pattern = { 'lua', 'javascript', 'vue', 'nix' },
-    command = 'setlocal shiftwidth=2 softtabstop=2 expandtab',
+  pattern = {
+    'lua',
+    'vue',
+    'nix',
+    'ts',
+    'javascript',
+    'javascriptreact',
+    'javascript.jsx',
+    'javascript.vue',
+    'typescript',
+    'typescriptreact',
+    'typescript.tsx',
+  },
+  command = 'setlocal shiftwidth=2 softtabstop=2 expandtab',
 })
 
-autocmd({"BufWritePre"}, {
-    group = group,
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
+autocmd({ "BufWritePre" }, {
+  group = group,
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
 })
