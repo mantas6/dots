28bcf267045008747285a9956dcd557ffbf3a2b8
Mantas <6524518+mantas6@users.noreply.github.com>
2026-02-16 11:00:21 +0200

    initial calc

--- home/.config/nvim/lua/plugins/lsp-shim.lua
+++ home/.config/nvim/lua/plugins/lsp-shim.lua
@@ -3,12 +3,41 @@ return {
 
   config = function()
     local null_ls = require("null-ls")
+    local h = require("null-ls.helpers")
+
+    local calc_ls = h.make_builtin({
+      name = "calc-ls",
+      method = null_ls.methods.DIAGNOSTICS,
+      filetypes = { "php" },
+      generator_opts = {
+        command = "calc-ls",
+        args = {},
+        to_stdin = true,
+        format = "json",
+        runtime_condition = function(params)
+          return params.bufname:match("%.calc%.php$") ~= nil
+        end,
+        on_output = function(params)
+          local diags = {}
+          for _, item in ipairs(params.output) do
+            table.insert(diags, {
+              row = item.line,
+              message = item.message,
+              severity = h.diagnostics.severities["hint"],
+            })
+          end
+          return diags
+        end,
+      },
+      factory = h.generator_factory,
+    })
 
     null_ls.setup({
       sources = {
         null_ls.builtins.diagnostics.phpstan,
         -- null_ls.builtins.formatting.pint,
         -- null_ls.builtins.formatting.shfmt,
+        calc_ls,
       },
     })
   end,
