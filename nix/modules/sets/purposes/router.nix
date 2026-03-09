1520d177916d075cc0f4b96530a6d6fdf10d5875
Mantas <6524518+mantas6@users.noreply.github.com>
2026-03-09 11:15:39 +0200

    disable

--- nix/modules/sets/purposes/router.nix
+++ nix/modules/sets/purposes/router.nix
@@ -88,9 +88,9 @@ in {
       };
 
       # Rebind dhcpcd on wan carrier restore (cable re-plug)
-      services.udev.extraRules = ''
-        ACTION=="change", SUBSYSTEM=="net", ENV{INTERFACE}=="${wanIfName}", ENV{CARRIER}=="1", RUN+="${pkgs.systemd}/bin/systemctl reload dhcpcd.service"
-      '';
+      # services.udev.extraRules = ''
+      #   ACTION=="change", SUBSYSTEM=="net", ENV{INTERFACE}=="${wanIfName}", ENV{CARRIER}=="1", RUN+="${pkgs.systemd}/bin/systemctl reload dhcpcd.service"
+      # '';
 
       networking = {
         interfaces = {
