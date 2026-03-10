37879ec6a86a8fd1344c293f45ec7de7b56fd9b3
Mantas <6524518+mantas6@users.noreply.github.com>
2026-03-10 10:06:48 +0200

    nvim: add gd

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -20,13 +20,7 @@ return {
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-        if client and client:supports_method('textDocument/inlayHint', event.buf) then
-          map('<leader>bh', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-          end, '[T]oggle Inlay [H]ints')
-        end
+        map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
       end,
     })
 
