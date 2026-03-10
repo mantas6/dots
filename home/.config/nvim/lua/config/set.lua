e17fdcf2729af08a913fe8fdf5ccca0c9549461b
Mantas <6524518+mantas6@users.noreply.github.com>
2026-03-10 10:08:47 +0200

    nvim: diagnostic

--- home/.config/nvim/lua/config/set.lua
+++ home/.config/nvim/lua/config/set.lua
@@ -35,6 +35,22 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.spell = true
 vim.opt.spelllang = 'en'
 
-vim.diagnostic.config({ virtual_text = true })
+vim.diagnostic.config({
+  update_in_insert = false,
+  severity_sort = true,
+  float = { border = 'square', source = 'if_many' },
+  underline = {
+    severity = {
+      min = vim.diagnostic.severity.WARN,
+    },
+  },
+
+  -- Can switch between these as you prefer
+  virtual_text = true, -- Text shows up at the end of the line
+  virtual_lines = false, -- Text shows up underneath the line, with virtual lines
+
+  -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
+  -- jump = { float = true },
+})
 
 -- vim.o.grepprg = 'set grepprg=rg --vimgrep --no-heading --smart-case'
