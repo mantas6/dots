3c3b97ef9f03fe99b34abfb09cd18f5c95dfdacf
Mantas <6524518+mantas6@users.noreply.github.com>
2026-03-11 09:35:21 +0200

    sessionizer init

--- opt/sessionizer/main.go
+++ opt/sessionizer/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	// "fmt"
+	"log"
+	"os"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	configText := getUserConfigurationText()
+
+	buildConfigurationObjects(configText)
+}
+
+func buildConfigurationObjects(configText string) {
+	//
+}
+
+func getUserConfigurationText() string {
+	configDir, err := os.UserConfigDir()
+
+	if err != nil {
+		log.Fatalf("Failed to get user configuration directory: %v", err)
+	}
+
+	cmd := exec.Command("yq", "-p", "toml", "-o", "json", ".", configDir+"/tmux/sessions.toml")
+	output, err := cmd.Output()
+
+	if err != nil {
+		log.Fatalf("Failed to read configuration file: %v", err)
+	}
+
+	return string(output)
 }
