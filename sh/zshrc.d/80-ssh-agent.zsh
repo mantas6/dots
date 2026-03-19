f57318536ef23f0619703d1007273775dee9abf5
Mantas <6524518+mantas6@users.noreply.github.com>
2026-03-19 08:43:58 +0200

    ssh agent on mac initial

--- sh/zshrc.d/80-ssh-agent.zsh
+++ sh/zshrc.d/80-ssh-agent.zsh
@@ -1,11 +1,21 @@
 #!/usr/bin/env zsh
 
-[ "$(uname)" = 'Darwin' ] && return
+# Add to .ssh/config for MacOS:
+#
+# IgnoreUnknown UseKeychain
+#
+# Host *
+#     AddKeysToAgent yes
+#     UseKeychain yes
 
-if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    ssh-agent -t 24h > "$XDG_RUNTIME_DIR/ssh-agent.env"
+if [[ "$(uname)" = 'Darwin' ]]; then
+    [[ ! -S "$SSH_AUTH_SOCK" ]] && export SSH_AUTH_SOCK="$(launchctl getenv SSH_AUTH_SOCK 2>/dev/null)"
+    ssh-add --apple-load-keychain >/dev/null 2>&1
+    return
 fi
 
-if [[ ! -f "$SSH_AUTH_SOCK" ]]; then
-    source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
+if ! pgrep -u "$USER" ssh-agent >/dev/null; then
+    ssh-agent -t 24h >"$XDG_RUNTIME_DIR/ssh-agent.env"
 fi
+
+[[ ! -S "$SSH_AUTH_SOCK" ]] && source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
