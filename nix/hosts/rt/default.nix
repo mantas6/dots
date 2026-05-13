41d9e3227b44357d510b7cc2e49ed0759a24370a
Mantas <6524518+mantas6@users.noreply.github.com>
2026-05-13 12:29:43 +0300

    disable serial console

--- nix/hosts/rt/default.nix
+++ nix/hosts/rt/default.nix
@@ -17,17 +17,17 @@
     # "services.docker"
   ];
 
-  boot.kernelParams = [
-    "console=ttyS0,115200n8"
-    "console=ttyS1,115200n8"
-    "console=ttyS2,115200n8"
-    "console=ttyS3,115200n8"
-  ];
-  boot.loader.grub.extraConfig = "
-   serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1
-   terminal_input serial
-   terminal_output serial
-    ";
+  # boot.kernelParams = [
+  #   "console=ttyS0,115200n8"
+  #   "console=ttyS1,115200n8"
+  #   "console=ttyS2,115200n8"
+  #   "console=ttyS3,115200n8"
+  # ];
+  # boot.loader.grub.extraConfig = "
+  #  serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1
+  #  terminal_input serial
+  #  terminal_output serial
+  #   ";
 
   networking.hostName = "rt";
 
