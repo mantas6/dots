dd8f00921f9a0264e8bd567b82772a54820d649d
Mantas <6524518+mantas6@users.noreply.github.com>
2026-05-20 11:47:59 +0300

    add telescope picker for harpoon

--- home/.config/nvim/lua/plugins/bookmarks.lua
+++ home/.config/nvim/lua/plugins/bookmarks.lua
@@ -17,6 +17,29 @@ return {
       },
     })
 
+    local conf = require('telescope.config').values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require('telescope.pickers')
+        .new({}, {
+          prompt_title = 'Harpoon',
+          finder = require('telescope.finders').new_table({
+            results = file_paths,
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        })
+        :find()
+    end
+
+    vim.keymap.set('n', '<leader>E', function()
+      toggle_telescope(harpoon:list())
+    end, { desc = 'Open harpoon window' })
+
     vim.keymap.set('n', '<leader>t', function()
       harpoon:list():add()
     end)
