6181a2fb57e79b5ab3f302704d3a655c3b41b3e5
Mantas <6524518+mantas6@users.noreply.github.com>
2026-06-24 10:51:12 +0300

    migrate to keyd from xremap

--- nix/modules/sets/collections/desktop/remap.nix
+++ nix/modules/sets/collections/desktop/remap.nix
@@ -11,19 +11,16 @@
         ids = ["*"];
 
         settings = {
-          # Caps Lock -> Left Control
           main = {
             capslock = "leftcontrol";
           };
 
-          # Left Alt + key -> word navigation
           alt = {
             left = "C-left";
             right = "C-right";
             backspace = "C-backspace";
           };
 
-          # Super + key -> line nav / browser / delete
           meta = {
             left = "home";
             right = "end";
