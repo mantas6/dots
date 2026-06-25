d85b7387c3ddca2fba4a1196c36b9f5822c897c4
Mantas <6524518+mantas6@users.noreply.github.com>
2026-06-25 11:32:57 +0300

    disable for now

--- nix/features/collections/desktop/audio.nix
+++ nix/features/collections/desktop/audio.nix
@@ -6,7 +6,7 @@
     services.pipewire = {
       enable = true; # if not already enabled
       alsa.enable = true;
-      alsa.support32Bit = true;
+      # alsa.support32Bit = true;
       pulse.enable = true;
       # If you want to use JACK applications, uncomment this
       #jack.enable = true;
