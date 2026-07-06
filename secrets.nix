17fd84d3cf456f11797b74ecd14c2f3222557519
Mantas <6524518+mantas6@users.noreply.github.com>
2026-07-06 09:19:34 +0300

    refactor(secrets): use attrset for systems keyed by host

--- secrets.nix
+++ secrets.nix
@@ -3,19 +3,21 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA9tV1mcJldS7nCldejKlFBtiL0Zm329wpHeccF8phEw mantas@a5"
   ];
 
-  systems = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO1l4E2BxsfN8rHZnntHirLssQQsQ+gofyrJYo+nMWz5 @mt"
-  ];
+  systems = {
+    mt = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO1l4E2BxsfN8rHZnntHirLssQQsQ+gofyrJYo+nMWz5";
+  };
+
+  allSystems = builtins.attrValues systems;
 
   basePath = "lib/secrets";
 in {
   "${basePath}/sat-base-url.age" = {
-    publicKeys = users ++ systems;
+    publicKeys = users ++ allSystems;
     armor = true;
   };
 
   "${basePath}/test-secret.age" = {
-    publicKeys = users ++ systems;
+    publicKeys = users ++ allSystems;
     armor = true;
   };
 }
