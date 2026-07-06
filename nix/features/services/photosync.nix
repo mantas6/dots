64f14b74f5f679815d3c57dc6f888921dbe772c2
Mantas <6524518+mantas6@users.noreply.github.com>
2026-07-06 10:49:52 +0300

    refactor(nix): extract sat-notify into nix/packages

--- nix/features/services/photosync.nix
+++ nix/features/services/photosync.nix
@@ -81,39 +81,6 @@
     pkgs,
     ...
   }: {
-    packages.sat-notify = pkgs.writeShellApplication {
-      name = "sat-notify";
-      runtimeInputs = [pkgs.curl pkgs.coreutils];
-      text = ''
-        message="$1"
-        expire='+2 days'
-
-        state_home=''${XDG_STATE_HOME:-"$HOME/.local/state"}
-        state=''${SAT_JOURNAL_STATE:-"$state_home/sat"}
-
-        if [ ! -f "$state/url" ]; then
-          echo 'Error: URL is not configured. Use sat-login command to enter it.' >&2
-          exit 1
-        fi
-
-        if [ ! -f "$state/token" ]; then
-          echo 'Error: Token is not configured' >&2
-          exit 1
-        fi
-
-        url=$(<"$state/url")
-        token=$(<"$state/token")
-
-        curl -fsSLX POST \
-          -d "message=$message" \
-          -d "expire=$expire" \
-          -H "Authorization: Bearer $token" \
-          "$url/api/notify"
-
-        exit 0
-      '';
-    };
-
     packages.photosync = pkgs.stdenv.mkDerivation {
       pname = "photosync";
       version = "0.1.0";
