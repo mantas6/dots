0fda5ccf84a703d369bd760e74e60faa2b523db3
Mantas <6524518+mantas6@users.noreply.github.com>
2026-07-07 09:09:58 +0300

    add rootless docker auto-prune timer

--- nix/features/services/docker.nix
+++ nix/features/services/docker.nix
@@ -15,5 +15,26 @@
     boot.kernel.sysctl = {
       "net.ipv4.ip_unprivileged_port_start" = 0;
     };
+
+    systemd.user.services.docker-prune = {
+      description = "Prune rootless docker resources";
+      after = ["docker.service"];
+      requires = ["docker.service"];
+      unitConfig.ConditionUser = "!root";
+      serviceConfig = {
+        Type = "oneshot";
+        Environment = "DOCKER_HOST=unix://%t/docker.sock";
+        ExecStart = "${lib.getExe config.virtualisation.docker.rootless.package} system prune -f";
+      };
+    };
+
+    systemd.user.timers.docker-prune = {
+      description = "Prune rootless docker resources";
+      wantedBy = ["timers.target"];
+      timerConfig = {
+        OnCalendar = "weekly";
+        Persistent = true;
+      };
+    };
   };
 }
