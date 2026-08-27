f422600934d56f92e8259de0bc7050dbf32cb487
Mantas <6524518+mantas6@users.noreply.github.com>
2026-08-27 08:30:26 +0300

    Add nerd font to l4 monitor

--- nix/hosts/l4/default.nix
+++ nix/hosts/l4/default.nix
@@ -97,6 +97,21 @@
 
     console.font = "ter-732n";
 
+    services.kmscon = {
+      enable = true;
+      hwRender = true;
+      fonts = [
+        {
+          name = "AnonymicePro Nerd Font Mono";
+          package = pkgs.nerd-fonts.anonymice;
+        }
+      ];
+      extraConfig = ''
+        font-engine=pango
+        font-size=30
+      '';
+    };
+
     networking.hostName = "l4";
 
     system.stateVersion = "25.05";
