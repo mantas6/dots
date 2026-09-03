7b29037ff3a18c664d2275ddc2c6827dbf53d2c2
Mantas <6524518+mantas6@users.noreply.github.com>
2026-09-03 08:31:21 +0300

    Split l4 config into dash and gallery modules
    Move kmscon/tty dashboard config to dash.nix (incl. psmouse blacklist
    to disable trackpad) and gallery service/caddy/udisks/exiftool to
    gallery.nix

--- nix/hosts/l4/default.nix
+++ nix/hosts/l4/default.nix
@@ -28,18 +28,10 @@
     features.swapSizeInGB = 8;
     powerManagement.powertop.enable = true;
 
-    environment.systemPackages = with pkgs; [
-      exiftool
-    ];
-
     services.caddy = {
       enable = true;
       # user = "mantas";
       virtualHosts = {
-        "http://gal".extraConfig = ''
-          reverse_proxy http://localhost:8079
-        '';
-
         "http://memos".extraConfig = ''
           reverse_proxy http://localhost:5230
         '';
@@ -48,68 +40,6 @@
         #   reverse_proxy http://localhost:8077
         # '';
       };
-    };
-
-    systemd.services.gallery = {
-      description = "Gallery";
-      wantedBy = ["multi-user.target"];
-      after = ["network.target"];
-      path = [pkgs.caddy];
-      script = ''
-        caddy run --config - --adapter caddyfile <<'EOF'
-          {
-            admin off
-          }
-
-          # :8077 {
-          #   root    * /home/mantas/Pictures/Nostalgia/Site
-          #   file_server
-          # }
-          #
-          # :8078 {
-          #   root    * /home/mantas/Pictures/Nostalgia/Originals
-          #   file_server
-          # }
-
-          :8079 {
-            root    * /home/mantas/Pictures/Gallery/Site
-            file_server
-          }
-
-          :8080 {
-            root    * /home/mantas/Pictures/Gallery/Originals
-            file_server
-          }
-        EOF
-      '';
-      serviceConfig = {
-        User = "mantas";
-        Restart = "always";
-        Type = "simple";
-      };
-      environment = {
-      };
-    };
-
-    services.udisks2.enable = true;
-    services.getty.autologinUser = "mantas";
-
-    console.font = "ter-732n";
-
-    services.kmscon = {
-      enable = true;
-      hwRender = true;
-      fonts = [
-        {
-          name = "AnonymicePro Nerd Font Mono";
-          package = pkgs.nerd-fonts.anonymice;
-        }
-      ];
-      extraConfig = ''
-        font-engine=pango
-        font-size=30
-        dpms-timeout=0
-      '';
     };
 
     networking.hostName = "l4";
