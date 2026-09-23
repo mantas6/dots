d73087f21b0439c52b227044ad99e92bb139ddf7
Mantas <6524518+mantas6@users.noreply.github.com>
2026-09-23 08:08:32 +0300

    Restrict sat-network secret to sat host and remove test secret

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,7 @@
 let
   users = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA9tV1mcJldS7nCldejKlFBtiL0Zm329wpHeccF8phEw mantas@ix"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKwmj+D1NO4kg3E6JH4ck0q+C65hTiTh69POfqXMROhF mantas@X13"
   ];
 
   systems = import ./nix/_lib/systems.nix;
@@ -15,12 +16,7 @@ in {
   };
 
   "${basePath}/sat-network.age" = {
-    publicKeys = users ++ allSystems;
-    armor = true;
-  };
-
-  "${basePath}/test-secret.age" = {
-    publicKeys = users ++ allSystems;
+    publicKeys = users ++ [systems.sat];
     armor = true;
   };
 }
